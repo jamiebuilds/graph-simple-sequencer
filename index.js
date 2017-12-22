20a12f8a946a656c742ae7a90c7b3b3b5036564c
James Kyle <me@thejameskyle.com>
2017-12-22 15:56:44 +1100

    Fix flow types

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 /*::
 type Graph<T> = Map<T, Array<T>>;
 type Chunks<T> = Array<Array<T>>;
+type Result<T> = { safe: boolean, chunks: Chunks<T> };
 */
 
-function graphSimpleSequencer/*::<T>*/(graph /*: Graph<T> */) /*: Chunks<T> */ {
+function graphSimpleSequencer/*::<T>*/(graph /*: Graph<T> */) /*: Result<T> */ {
   let chunks = [];
   let safe = true;
   let queue = new Set(graph.keys());
@@ -14,7 +15,7 @@ function graphSimpleSequencer/*::<T>*/(graph /*: Graph<T> */) /*: Chunks<T> */ {
     let current = new Map();
 
     for (let key of queue) {
-      let deps = graph.get(key);
+      let deps = graph.get(key) || [];
       let curr = deps.filter(dep => queue.has(dep));
 
       current.set(key, curr);
@@ -29,13 +30,13 @@ function graphSimpleSequencer/*::<T>*/(graph /*: Graph<T> */) /*: Chunks<T> */ {
       let items = Array.from(queue);
 
       let sorted = items.sort((a, b) => {
-        let aCurr = current.get(a);
-        let bCurr = current.get(b);
+        let aCurr = current.get(a) || [];
+        let bCurr = current.get(b) || [];
         let deps = aCurr.length - bCurr.length;
         if (deps !== 0) return deps;
 
-        let aChildren = items.filter(item => current.get(item).includes(a));
-        let bChildren = items.filter(item => current.get(item).includes(b));
+        let aChildren = items.filter(item => (current.get(item) || []).includes(a));
+        let bChildren = items.filter(item => (current.get(item) || []).includes(b));
         return bChildren.length - aChildren.length;
       });
 
